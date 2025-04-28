16a8be29c18c90bd1c5a63a1b4d21581019e6414
choisujin96 <csjin9783@naver.com>
2025-04-28 16:56:18 +0900

    2025-04-28 객체지향

---
+++ src/com/javaex/ex01/Goods.java
@@ -0,0 +1,19 @@
+package com.javaex.ex01;
+
+public class Goods {
+	
+	// 이번 class는 틀임.
+
+	
+	
+	//필드
+	String name;
+	int price;
+	
+	
+	
+	//생성자
+	//메소드-gs,일반
+	
+
+}
---
+++ src/com/javaex/ex01/GoodsApp.java
@@ -0,0 +1,42 @@
+package com.javaex.ex01;
+
+public class GoodsApp {
+
+	//시나리오 짜는 곳
+	public static void main(String[] args) {
+		//main이 붙은 애가 첫번째
+		
+		Goods camera = new Goods();
+		camera.name="니콘";
+		camera.price=400000;
+		
+		System.out.println(camera.name);
+		System.out.println(camera.price);
+		
+		System.out.println("======================");
+		
+		Goods cup = new Goods();
+		cup.name="머그컵";
+		cup.price=2000;
+		
+		System.out.println(cup.name);
+		System.out.println(cup.price);
+
+		System.out.println("======================");
+		
+		Goods computer = new Goods();
+		computer.name="LG그램";
+		computer.price=1000000;		
+		
+		System.out.println(computer.name);
+		System.out.println(computer.price);
+		
+		System.out.println("======================");
+		
+		System.out.println(camera.price);
+		
+		
+		
+	}
+
+}
